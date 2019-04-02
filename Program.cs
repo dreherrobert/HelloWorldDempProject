309d0432993eab0a275e0e3d45f5f6f2a01cbaee
Robert Dreher <rd@basl.at>
2019-04-02 12:09:55 +0200

    (Steip 1) eigene Exceptionklasse

--- Program.cs
+++ Program.cs
@@ -10,8 +10,37 @@ namespace HelloWorldDempProject
     {
         static void Main(string[] args)
         {
-            var teilnehmer = "Teilnehmer 1";
-            Console.WriteLine($"Hallo Sage {teilnehmer}");
+            //Console.WriteLine(args.Length);
+
+            //Console.Read();
+            //var teilnehmer = "Teilnehmer 1";
+            try
+            {
+                Console.WriteLine($"Hallo Sage {args[1]}!");
+                //Console.WriteLine("Super, kein Fehler.");
+            }
+            //catch(IndexOutOfRangeException ioore)
+            //{
+            //    Console.WriteLine(ioore.Message);
+            //}
+            catch (Exception)
+            {
+
+                throw new DemoException();
+            }
+            Console.WriteLine("Super, kein Fehler");
+        }
+    }
+
+    public class DemoException : Exception
+    {
+        public DemoException(string message)
+        {
+
+        }
+        public DemoException()
+        {
+
         }
     }
 }
