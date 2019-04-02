1b36e932826a154db932a479bba78eb74bd2491e
Robert Dreher <rd@basl.at>
2019-04-02 10:49:33 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldDempProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var teilnehmer = "Teilnehmer 1";
+            Console.WriteLine($"Hallo Sage {teilnehmer}");
+        }
+    }
+}
