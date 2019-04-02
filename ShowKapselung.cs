5a68d67ec8290141811560cd497d298f03046a8f
Robert Dreher <rd@basl.at>
2019-04-02 17:28:08 +0200

    Kapselung, Vererbung

---
+++ ShowKapselung.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldDempProject
+{
+    class ShowKapselung
+    {
+        public void TestA()
+        {
+
+        }
+
+        private void TestB()
+        {
+
+        }
+        internal void TestC()
+        {
+
+        }
+
+        
+    }
+
+    public class Child : Parent
+    {
+        readonly private Parent _parent = new Parent();
+
+
+        public Child()
+        {
+            ParentMethodA();
+            ParentMethodC();
+        }
+
+        public override void ParentMethodC()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Parent
+    {
+        protected void ParentMethodA() { }
+
+        private void ParentMethodB() { }
+
+        public abstract void ParentMethodC();
+        
+        virtual public void ParentMethodD()
+        {
+
+
+        }
+    }
+}
---
+++ ShowKapselungTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldDempProject
+{
+    [TestClass]
+    class ShowKapselungTests
+    {
+        [TestMethod]
+        public void DemoKapselung()
+        {
+            var mock = new ShowKapselung();
+            mock.TestA();
+            mock.TestC();
+
+            var child = new Child();
+
+            child.ParentMethodD();
+        }
+    }
+}
